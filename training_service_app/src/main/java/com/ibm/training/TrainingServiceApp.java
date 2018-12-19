ce95a0745546a134743fdcf393076a35a2d52071
saketkashyap <sakkashy@in.ibm.com>
2018-12-19 12:02:01 +0530

    connection with eureka established

--- training_service_app/src/main/java/com/ibm/training/TrainingServiceApp.java
+++ training_service_app/src/main/java/com/ibm/training/TrainingServiceApp.java
@@ -5,12 +5,14 @@ package com.ibm.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author saket
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrainingServiceApp {
 
 	/**
